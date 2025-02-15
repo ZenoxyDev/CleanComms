df9d1092623a101bebb3c0f0fd8733dce1845d71
Zenoxy <dmrailesi005@gmail.com>
2025-02-16 01:50:47 +0300

    Initial commit

---
+++ extension.js
@@ -0,0 +1,42 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand('cleancomms.removeComments', function () {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      vscode.window.showInformationMessage('Lütfen bir dosya açın!');
+      return;
+    }
+
+    const document = editor.document;
+    const fullText = document.getText();
+    
+    const commentRegex = /(\/\/.*$|\/\*[\s\S]*?\*\/)/gm;
+
+    const newText = fullText.replace(commentRegex, '');
+    const fullRange = new vscode.Range(
+      document.positionAt(0),
+      document.positionAt(fullText.length)
+    );
+
+    editor.edit(editBuilder => {
+      editBuilder.replace(fullRange, newText);
+    }).then(success => {
+      if (success) {
+        vscode.window.showInformationMessage('Tüm yorum satırları silindi!');
+      } else {
+        vscode.window.showErrorMessage('Yorumlar silinemedi.');
+      }
+    });
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
